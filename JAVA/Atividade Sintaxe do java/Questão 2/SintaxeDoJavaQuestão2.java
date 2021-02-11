65f9a70bd0760afafc20c5c6b0e07a3297c893f3
Diego Ramos Brito <diegorbrito9@gmail.com>
2021-02-11 13:23:44 -0300

    Sintaxe do java
    Questão 2

---
+++ JAVA/Atividade Sintaxe do java/Questão 2/SintaxeDoJavaQuestão2.java
@@ -0,0 +1,35 @@
+/*
+* 
+*  @author Diego Brito
+*
+*/
+import java.util.Scanner;
+public class SintaxeDoJavaQuestão2 {
+
+    public static void main(String[] args) {
+        
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+       
+           
+        while (i == 0){
+            
+            System.out.println("Você vai passar em ds1 ?");
+            String op = sc.nextLine();
+            if ("s".equals(op)){
+                System.out.println("QUE BOM !");
+                i++;
+            }
+            else if("n".equals(op)){
+                System.out.println("Acredite em você !");
+                i++;
+            } 
+            else if (!"s".equals(op) || !"n".equals(op)){
+                System.out.println("Digite 's' ou 'n' ");
+                i=0;
+                System.out.println("\n");
+            }  
+        } 
+    }
+    
+}
