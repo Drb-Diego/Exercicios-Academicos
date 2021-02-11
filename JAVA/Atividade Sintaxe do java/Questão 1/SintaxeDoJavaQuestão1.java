cc41af0e8db9c755c3c86f0418c4909c10852110
Diego Ramos Brito <diegorbrito9@gmail.com>
2021-02-11 13:21:01 -0300

    Atividade Sintaxe do Java
    Questão 1

---
+++ JAVA/Atividade Sintaxe do java/Questão 1/SintaxeDoJavaQuestão1.java
@@ -0,0 +1,21 @@
+/*
+* 
+*    @author Diego Brito
+*/
+public class SintaxeDoJavaQuestão1 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i<20; i++){
+           if (i!=0){
+               System.out.println(i);
+           }
+        }     
+        
+    }
+
+    /*
+        Escreva um programa (usando repetição) que mostre apenas os números ímpares entre 1 e 19. No fim, deve mostrar uma mensagem de fim de programa
+    */
+    
+}
