c9ed32bea25264483d85b7d4ba108b464cf2bb51
Diego Ramos Brito <69371037+Drb-Diego@users.noreply.github.com>
2021-02-11 13:48:19 -0300

    Colocando Descrição da atividade

--- JAVA/Atividade Sintaxe do java/Questão 2/SintaxeDoJavaQuestão2.java
+++ JAVA/Atividade Sintaxe do java/Questão 2/SintaxeDoJavaQuestão2.java
@@ -4,7 +4,6 @@
 */
 import java.util.Scanner;
 public class SintaxeDoJavaQuestão2 {
-
     public static void main(String[] args) {
         
         int i = 0;
@@ -30,5 +29,12 @@ public class SintaxeDoJavaQuestão2 {
             }  
         } 
     }
+  
+  /*
+    Escreva um programa que pergunte “Você vai passar em DS1?”, sendo duas opções possíveis: S e N. 
+    Caso o usuário digite S, mostre uma mensagem positiva. Caso o usuário digite N, mostre uma mensagem de incentivo. 
+    O programa deve continuar pedindo para o usuário digitar até que ele coloque S ou N.
+
+  */
     
 }
